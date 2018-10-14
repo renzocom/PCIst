34c7ff1057031c1780eb9e9b3be852213116ee07
Renzo Comolatti <renzo.com@gmail.com>
2018-10-14 17:55:32 -0300

    Update PCIst.py

--- PCIst.py
+++ PCIst.py
@@ -1,5 +1,10 @@
+#
+# Renzo Comolatti (renzo.com@gmail.com) and Adenauer G. Casali
+#
+# See Comolatti et al., "A fast and general method to empirically estimate the complexity of distributed causal interactions in the brain"
+#
 # started: 26/10/2017
-# last update: 01/08/2018
+# last update: 09/10/2018
 
 import numpy as np
 from numpy import linalg
@@ -9,7 +14,6 @@ import time
 
 def calc_PCIst(signal_evk, times, **par):
     ''' Calculates PCIst (Perturbational Complexity Index based on State transitions) of a signal.
-
     Parameters
     ----------
     signal_evk : ndarray
@@ -32,13 +36,15 @@ def calc_PCIst(signal_evk, times, **par):
     signal_evk, times = preprocess_signal(signal_evk, times, (par['baseline_window'][0],par['response_window'][1]), **par)
     signal_svd, eigenvalues = dimensionality_reduction(signal_evk, times, **par)
     results = state_transition_quantification(signal_svd, times, **par)
+
+
     return results
 
 ## DIMENSIONALITY REDUCTION
-def dimensionality_reduction(signal, times, response_window, max_var=99, min_snr=1.1, **kwargs):
+def dimensionality_reduction(signal, times, response_window, max_var=0.99, min_snr=1.1, n_components=None, **kwargs):
     '''Returns principal components of signal according to SVD of the response.
 
-    Calculates SVD at a given time interval (t>0) and uses the new basis to transform the whole signal yielding `n_components` principal components.
+    Calculates SVD at a given time interval (response_window) and uses the new basis to transform the whole signal yielding `n_components` principal components.
     The principal components are then selected to account for at least `max_var`% of the variance basesent in the signal's response.
 
     Parameters
@@ -46,15 +52,16 @@ def dimensionality_reduction(signal, times, response_window, max_var=99, min_snr
     signal : ndarray
         2D array (ch,time) containing signal.
     times : ndarray
-        1D array (time,) containing timepoints (negative values are baseline).
+        1D array (time,) containing timepoints
     response_window : tuple
-        Tuple (t_ini,t_end) with time interval of the response on which SVD is calculated.
-    max_var : 0 < float <= 100
+        Signal's response time interval (ini,end).
+    max_var: 0 < float <= 100
         Percentage of variance accounted for by the selected principal components.
     min_snr : float, optional
-        Selects principal components with a signal-to-noise ratio (SNR) > min_snr. If not given, SNR test is not performed.
+        Selects principal components with a signal-to-noise ratio (SNR) > min_snr.
     n_components : int, optional
         Number of principal components calculated (before selection).
+
 
     Returns
     -------
@@ -64,8 +71,8 @@ def dimensionality_reduction(signal, times, response_window, max_var=99, min_snr
         1D array (n_components,) with `n_components` SVD eigenvalues of the signal's response.
     '''
 
-
-    n_components = signal.shape[0]
+    if not n_components:
+        n_components = signal.shape[0]
 
     Vk, eigenvalues = get_svd(signal, times, response_window, n_components)
 
@@ -77,32 +84,30 @@ def dimensionality_reduction(signal, times, response_window, max_var=99, min_snr
 
     if min_snr:
         base_ini_ix, base_end_ix = get_time_index(times, kwargs['baseline_window'][0]), get_time_index(times, kwargs['baseline_window'][1])
-        resp_ini_ix, resp_end_ix = get_time_index(times, kwargs['response_window'][0]), get_time_index(times, kwargs['response_window'][1])
+        resp_ini_ix, resp_end_ix = get_time_index(times, response_window[0]), get_time_index(times, response_window[1])
         n_dims=np.size(signal_svd,0)
         snrs=np.zeros(n_dims)
         for c in range(n_dims):
-            snrs[c]=np.sqrt((np.divide(np.mean(np.square(signal_svd[c,resp_ini_ix:resp_end_ix])),np.mean(np.square(signal_svd[c,base_ini_ix:base_end_ix])))))
+            snrs[c]=np.sqrt((np.divide(np.mean(np.square(signal_svd[c,resp_ini_ix:resp_end_ix])),
+                                       np.mean(np.square(signal_svd[c,base_ini_ix:base_end_ix])))))
         signal_svd = signal_svd[snrs>min_snr,:]
 
     return signal_svd, eigenvalues
+
 
 def get_svd(signal_evk, times, response_window, n_components):
     ini_t, end_t = response_window
     ini_ix = get_time_index(times, onset=ini_t)
     end_ix = get_time_index(times, onset=end_t)
-
-    signal_resp = signal_evk[:,ini_ix:end_ix].T # we want the matrix samples x dimensions, that is, time x channels
-
+    signal_resp = signal_evk[:,ini_ix:end_ix].T
     U, S, V = linalg.svd(signal_resp, full_matrices=False)
     V = V.T
     Vk = V[:,:n_components]
     eigenvalues = S[:n_components]
-
     return Vk, eigenvalues
 
 def apply_svd(signal, V):
     '''Transforms signal according to SVD basis.'''
-
     return signal.T.dot(V).T
 
 ## STATE TRANSITION QUANTIFICATION
@@ -114,7 +119,7 @@ def state_transition_quantification(signal, times, k, baseline_window, response_
     signal : ndarray
         2D array (component,time) containing signal (typically, the selected principal components).
     times : ndarray
-        1D array (time,) containing timepoints (negative values are baseline).
+        1D array (time,) containing timepoints
     k : float > 1
         Noise control parameter.
     baseline_window : tuple
@@ -144,7 +149,6 @@ def state_transition_quantification(signal, times, k, baseline_window, response_
         print('No components --> PCIst=0')
         return 0, []
 
-
     # EMBEDDING
     if embed:
         cut = (L-1)*tau
@@ -157,10 +161,7 @@ def state_transition_quantification(signal, times, k, baseline_window, response_
     else:
         signal = signal[:,np.newaxis,:]
 
-
-
-
-      # BASELINE AND RESPONSE DEFINITION
+    # BASELINE AND RESPONSE DEFINITION
     base_ini_ix, base_end_ix = get_time_index(times, baseline_window[0]), get_time_index(times, baseline_window[1])
     resp_ini_ix, resp_end_ix = get_time_index(times, response_window[0]), get_time_index(times, response_window[1])
 
@@ -173,32 +174,22 @@ def state_transition_quantification(signal, times, k, baseline_window, response_
     response = signal[:,:,resp_ini_ix:resp_end_ix]
 
     # NST CALCULATION
-
-    # Distance matrix
+        # Distance matrix
     D_base = np.zeros((n_dims,n_baseline,n_baseline))
     D_resp = np.zeros((n_dims,n_response,n_response))
-    # Transition matrix
+        # Transition matrix
     T_base = np.zeros((n_steps,n_dims,n_baseline,n_baseline))
     T_resp = np.zeros((n_steps,n_dims,n_response,n_response))
-
-    # Number of mean state transitions
+        # Number of state transitions
     NST_base = np.zeros((n_steps,n_dims))
     NST_resp = np.zeros((n_steps,n_dims))
-
     thresholds = np.zeros((n_steps,n_dims))
-
     for i in range(n_dims):
         D_base[i,:,:] = recurrence_matrix(baseline[i,:,:], thr=None, mode='distance')
         D_resp[i,:,:] = recurrence_matrix(response[i,:,:], thr=None, mode='distance')
-
         min_thr = np.median(D_base[i,:,:].flatten())
-        # max_thr = np.max(D_base[i,:,:].flatten()) * 2
-        #min_thr = 0
         max_thr = np.max(D_resp[i,:,:].flatten()) * max_thr_p
-        # # max_thr = np.max(D_base[i,:,:].flatten())
-        # max_thr = np.max([1,2,3])
         thresholds[:,i] = np.linspace(min_thr, max_thr, n_steps)
-
     for i in range(n_steps):
         for j in range(n_dims):
             T_base[i,j,:,:] = distance2transition(D_base[j,:,:],thresholds[i,j])
@@ -207,13 +198,12 @@ def state_transition_quantification(signal, times, k, baseline_window, response_
             NST_base[i,j] = np.sum(T_base[i,j,:,:])/n_baseline**2
             NST_resp[i,j] = np.sum(T_resp[i,j,:,:])/n_response**2
 
+    # PCIST
     NST_diff = NST_resp - k * NST_base
     ixs = np.argmax(NST_diff,axis=0)
-
     max_thresholds = np.array([thresholds[ix,i] for ix,i in zip(ixs,range(n_dims))])
     PCI_bydim = np.array([NST_diff[ix,i] for ix,i in zip(ixs,range(n_dims))]) * n_response
     PCI = np.sum(PCI_bydim)
-
 
     temp = np.zeros((n_dims,n_response,n_response))
     temp2 = np.zeros((n_dims,n_baseline,n_baseline))
@@ -278,7 +268,6 @@ def distance2recurrence(distR, thr):
     return mask.astype(int)
 
 def diff_matrix(A, symmetric=False):
-    #grad_A = np.array(np.gradient(A))
     B = np.abs(np.diff(A))
     if B.shape[1] != B.shape[0]:
         B2 = np.zeros((B.shape[0],B.shape[1]+1))
@@ -304,7 +293,6 @@ def calc_maxdim(eigenvalues, max_var):
 def dimension_embedding(x, L, tau):
     '''
     Returns time-delay embedding of vector.
-
     Parameters
     ----------
     x : ndarray
@@ -319,12 +307,11 @@ def dimension_embedding(x, L, tau):
         2D array containing embedded signal (L, time)
 
     '''
-
     assert len(x.shape)==1, "x must be one-dimensional array (n,)"
-    n = x.shape[0] # length of time-series
+    n = x.shape[0]
     s = np.zeros((L,n - (L-1)*tau))
     ini = (L-1)*tau if L>1 else None
-    s[0,:] = x[ini:] # dimension "1"
+    s[0,:] = x[ini:]
     for i in range(1,m):
         ini = (L-i-1)*tau
         end = -i*tau
@@ -332,23 +319,17 @@ def dimension_embedding(x, L, tau):
     return s
 
 ## PREPROCESS
-
 def preprocess_signal(signal_evk, times, time_window, baseline_corr=False, resample=None, **kwargs):
-
     assert signal_evk.shape[1] == len(times), 'Signal and Time arrays must be of the same size.'
-
     if baseline_corr:
         signal_evk = baseline_correct(signal_evk, times, delta=-50)
-
     t_ini, t_end = time_window
     ini_ix = get_time_index(times,t_ini)
     end_ix = get_time_index(times,t_end)
     signal_evk = signal_evk[:,ini_ix:end_ix]
     times = times[ini_ix:end_ix]
-
     if resample:
         signal_evk, times = undersample_signal(signal_evk, times, new_fs=resample)
-
     return signal_evk, times
 
 def undersample_signal(signal, times, new_fs):
@@ -357,7 +338,6 @@ def undersample_signal(signal, times, new_fs):
     times : (times,) [ms]
     new_fs : [hz]
     '''
-
     n_samples = int((times[-1]-times[0])/1000 * new_fs)
     new_signal_evk, new_times = scipy.signal.resample(signal, n_samples, t=times, axis=1)
     return new_signal_evk, new_times
@@ -366,13 +346,12 @@ def baseline_correct(Y, times, delta=0):
     ''' Baseline correct signal using times < delta '''
     newY = np.zeros(Y.shape)
     onset_ix = get_time_index(times, delta)
-    baseline_mean = np.mean(Y[:,:onset_ix],axis=1)[np.newaxis] # shape: (1,dls)
+    baseline_mean = np.mean(Y[:,:onset_ix],axis=1)[np.newaxis]
     newY = Y - baseline_mean.T
     assert np.all(np.isclose(np.mean(newY[:,:onset_ix],axis=1),0,atol=1e-08)), "Baseline mean is not zero"
     return newY
 
 def get_time_index(times, onset=0):
     ''' Returns index of first time greater then delta. For delta=0 gets index of first non-negative time
-       OBS: alternative way is to find index of closest value to onset using np.argmin(np.abs(times - onset))
     '''
     return np.sum(times < onset)
